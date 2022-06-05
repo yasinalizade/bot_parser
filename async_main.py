abaa825a4065dd2ac479b0c2e034041ca4223e3e
Ясин Ализаде <yasin.alizade@yandex.ru>
2022-06-05 12:48:21 +0300

    Add logging

--- async_main.py
+++ async_main.py
@@ -2,10 +2,25 @@ import aiohttp
 import aiofiles
 import asyncio
 import datetime
+import logging
 
 from aiocsv import AsyncWriter
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(
+    filename='bot_parser.log',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    )
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler('bot_parser.log', maxBytes=500000, backupCount=2)
+logger.addHandler(handler)
+
+URL = 'https://lidl.ie/super-savers'
 
 
 async def collect_data():
@@ -21,28 +36,17 @@ async def collect_data():
         'User-Agent': ua.random
     }
 
-    url = 'https://lidl.ie/super-savers'
-
     async with aiohttp.ClientSession() as session:
-        response = await session.get(url, headers=headers)
+        response = await session.get(URL, headers=headers)
         soup = BeautifulSoup(await response.text(), "lxml")
         cards = soup.find_all('div', class_='nuc-a-flex-item--width-6')
 
         DATA = []
+
         for card in cards:
             card_title = card.find(
                 'h3',
                 class_='ret-o-card__headline').text.strip()
-
-            try:
-                card_discount = card.find(
-                    'div',
-                    class_='lidl-m-pricebox__discount-wrapper').text.strip()
-                card_discount = card_discount[
-                    card_discount.find('-'):card_discount.find('%')+1
-                ]
-            except AttributeError:
-                continue
 
             card_old_price = card.find(
                 'div',
@@ -61,15 +65,34 @@ async def collect_data():
                 class_='lidl-m-ribbon-item__text'
             ).text.replace(' ', '').strip()
 
-            DATA.append(
-                [
+            try:
+                card_discount = card.find(
+                    'div',
+                    class_='lidl-m-pricebox__discount-wrapper').text.strip()
+                card_discount = card_discount[
+                    card_discount.find('-'):card_discount.find('%') + 1
+                    ]
+            except AttributeError:
+                logging.error(f'{card_title} has no % in card, '
+                              'I will compute right now')
+                total_percent = 100
+                old_price = float(''.join(
+                        [i for i in card_old_price if i.isdigit() or i == '.'])
+                        )
+                sale_price = float(card_sale_price[1:])
+                card_discount = '-' + str(int((old_price - sale_price) /
+                                          old_price * total_percent)) + '%'
+
+            product = [
                     card_title,
                     card_old_price,
                     card_discount,
                     card_sale_price,
                     card_sale_date
                 ]
-            )
+
+            if product not in DATA:
+                DATA.append(product)
 
     async with aiofiles.open(f'LIDL_{cur_time}.csv', 'w') as file:
         writer = AsyncWriter(file)
